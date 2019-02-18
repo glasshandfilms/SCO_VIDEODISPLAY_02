0120534c95def979bd9516320ec84146691bd056
Brandon <brandon@glasshandstudios.com>
2019-02-18 16:44:26 -0500

    Initial Push

---
+++ Sco_VideoDisplay_02/Assets/Scripts/VideoCtrl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+[RequireComponent(typeof(VideoPlayer))]
+
+public class VideoCtrl : MonoBehaviour
+{
+    public VideoClip[] vids;
+    public Text currentMinutes;
+    public Text currentSeconds;
+    public Text totalMinutes;
+    public Text totalSeconds;
+    
+
+    private VideoPlayer videoPlayer;
+    private int videoClipIndex;
+
+    void Awake()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+    }
+
+    void Start()
+    {
+        videoPlayer.clip = vids[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(videoPlayer.isPlaying)
+        {
+            
+        }
+    }
+
+    
+
+
+
+}
